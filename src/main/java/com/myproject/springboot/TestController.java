df9fe29f263de30233877532a36d697e5783766b
sishamo <ryo.yomon@gmail.com>
2017-10-17 10:44:40 +0900

    First commit

---
+++ src/main/java/com/myproject/springboot/TestController.java
@@ -0,0 +1,12 @@
+package com.myproject.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping
+	public String index(){
+		return "Hello world!!";
+	}
+}
